21d10b622d0099d0d9fa0e42b41579e7f7221769
David <davidsanchezr81@gmail.com>
2019-05-06 22:52:24 +0100

    SumAreas and SumPerimeter removen from rectangle class and created separated clases for each of them. Single responsibility principle

---
+++ SolidPrinciples/SolidPrinciples/AreaOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolidPrinciples
+{
+    public class AreaOperations
+    {
+          public double Sum(IEnumerable<Rectangle> rectangles)
+          {
+                double area = 0;
+                foreach (var rectangle in rectangles)
+                    area += rectangle.Height * rectangle.Width;
+                return area;
+
+        }
+    }
+}
---
+++ SolidPrinciples/SolidPrinciples/PerimeterOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolidPrinciples
+{
+    public class PerimeterOperations
+    {
+        public static double Sum(IEnumerable<Rectangle> rectangles)
+
+        {
+            double perimeter = 0;
+            foreach (var rectangle in rectangles)
+                perimeter += 2 * rectangle.Height + 2 * rectangle.Width;
+            return perimeter;
+        }
+    }
+}
