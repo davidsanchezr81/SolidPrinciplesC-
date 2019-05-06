a08802dcb07addf6538615266fa4489fb5e6e26c
David <davidsanchezr81@gmail.com>
2019-05-06 22:38:39 +0100

    Main Class Created

--- SolidPrinciples/SolidPrinciples/Program.cs
+++ SolidPrinciples/SolidPrinciples/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SolidPrinciples
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rectangulos = new[]
+            {
+
+                new Rectangle {Width = 10, Height = 5},
+                new Rectangle {Width = 4, Height = 6},
+                new Rectangle {Width = 5, Height = 1},
+                new Rectangle {Width = 8, Height = 9},
+            };
+
+            var sumaDeAreas = Rectangle.SumAreas(rectangulos);
+            var sumaDePerimetros = Rectangle.SumPerimeters(rectangulos);
+
+            Console.WriteLine($"Areas total: {sumaDeAreas} " + $"\nPerimetro total:{sumaDePerimetros}");
+
+            Console.ReadKey();
         }
     }
 }
